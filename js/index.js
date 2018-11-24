744c688c0c93e44340a99df6b18df0a1719c0202
leberechttask <leberechttask@gmail.com>
2018-11-25 03:32:43 +0900

    re-modified

--- js/index.js
+++ js/index.js
@@ -2,4 +2,7 @@ $(function() {
     $('#system-btn').click(function() {
         window.location.href='pages/system.html';
     });
+    $('#system-name').click(function() {
+        window.location.reload();
+    });
 });
